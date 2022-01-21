a631c3b73f7c20766d595277dfb5721798b6e54c
SlappyMrFlappy <90064185+SlappyMrFlappy@users.noreply.github.com>
2022-01-20 21:13:24 -0500

    Update PID.cpp

--- DriveTrainMK3/PID.cpp
+++ DriveTrainMK3/PID.cpp
@@ -1,1 +1,33 @@
+# include "PID.h"
 
+// Constructors
+PID::PID(double _Sp, double _Dt, double _Kp, double _Ki, double _Kd):
+  Sp(_Sp), Dt(_Dt), Integral(0), pErr(0), Kp(_Kp), Ki(_Ki), Kd(_Kd)
+{
+  // intentionally blank
+}
+
+double PID::controlFunc(double Pv) {
+
+  // Calculate Error
+  double Err = Sp - Pv;
+
+  // Calculate Proportional term
+  double Pp = Kp * Err;
+
+  // Calculate Integral term
+  Integral += Err * Dt;
+  double Ip = Ki * Integral;
+
+  // Calculate Derivative term
+  double Derivative = (Err - pErr) / Dt;
+  double Dp = Kd * Derivative;
+
+  // Sum the individual terms
+  double Ut = Pp + Ip + Dp;
+
+  // Save current Error
+  pErr = Err;
+
+  return Ut;
+}
