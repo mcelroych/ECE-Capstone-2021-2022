0bc8905c1a53d0211056174f26ecb6e1a688ef67
SlappyMrFlappy <90064185+SlappyMrFlappy@users.noreply.github.com>
2022-02-26 08:35:36 -0500

    Add files via upload

--- mainCode.ino
+++ mainCode.ino
@@ -1,213 +1,192 @@
-#include <Wire.h>
-#include <Adafruit_PWMServoDriver.h>
+#include <Servo.h>
 
-// called this way, it uses the default address 0x40
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+Servo rotate;
+Servo upDown;
+Servo screen;
+Servo launcherServo;  // create servo object to control a servo
 
+#define LA_PWM_PIN       2
+#define ROTATE_SERVO_PIN 3
+#define UPDOWN_SERVO_PIN 4
+#define SCREEN_SERVO_PIN 7
+#define LA_DIR_PIN       23
 
-//FIX THESE VALUES BY CALIBRATING           
-#define SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
+#define ROTATE_START 173
+#define ROTATE_CUP   65
+#define ROTATE_STEPS ROTATE_START - ROTATE_CUP
 
-#define USMINROD  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
-#define USMAXROD  1700 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
-#define USDROPROD  900
-#define USSTRAIGHTROD  1300
-#define USVERTICALROD 2000
+#define UPDOWN_START 10
+#define UPDOWN_DUMP  37
+#define UPDOWN_HOLD  100
+#define UPDOWN_TOP   155
+#define UPDOWN_STEPS UPDOWN_TOP - UPDOWN_HOLD
 
-#define USMINSWIVEL 2200
-#define USMAXSWIVEL 2200
-#define USDROPSWIVEL 1150
+#define LA_UP_DIR   1
+#define LA_DOWN_DIR 0
 
-#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+#define LAUNCH_VALUE 2000
+#define ENCODER_PIN 21
+#define SERVO_PIN 5
+#define WINCH_PIN 6
+#define WINCH_DIR_PIN 26
+#define LIMIT_SW_PIN 24
 
-#define START_BUTTON 12
+#define FIRE_PIN  30
+#define ARM_GRAB  31
+#define READY     32
+#define START_BTN 52
+#define DT_ENABLE 53
 
+volatile int encoderValue = 0;
+bool armed = false;
 
-uint8_t servonum5 = 5;
-uint8_t servonum4 = 4;
-uint8_t servonum3 = 3;
-uint8_t servonum2 = 2;
-uint8_t servonum1 = 1;
-uint8_t servonum = 0;
-//*****************setup*****************************begin
 void setup() {
-  Serial.begin(9600);
-  Serial.println("8 channel Servo test!");
+  // put your setup code here, to run once:
+  armInit();
+  screenInit();
+  launcherInit();
 
-  pinMode(2, OUTPUT);// DIR pin for LA
-  pinMode(3, OUTPUT);// DIR pin for launcher winch
-  pinMode(13, OUTPUT);// mega led
-  pinMode(8, OUTPUT);// Drivetrain Start signal
-  pinMode(START_BUTTON, INPUT_PULLUP);
-
-  digitalWrite(13, LOW);
-  digitalWrite(13, LOW);
+  pinMode(DT_ENABLE, OUTPUT);
+  pinMode(FIRE_PIN, INPUT);
+  pinMode(READY, OUTPUT);
+  pinMode(ARM_GRAB, OUTPUT);
+  pinMode(START_BTN, INPUT);
+  while(!digitalRead(START_BTN));
   
-  //Start button
+  screenMove();
+  launcherArm();
+  //armPrep();
+  digitalWrite(DT_ENABLE, HIGH);  
+  
+  while(digitalRead(FIRE_PIN));
+  launcherFire();
   delay(500);
-  while(digitalRead(START_BUTTON)){} //holds the program until pin 8 is pulled low by the button
-  digitalWrite(13, HIGH);
+  launcherArm();
+  
+  digitalWrite(READY, HIGH);
   delay(500);
-  digitalWrite(13, LOW);
-  //Start button
-
-  
-
-  initServo();
-  moveScreen();//need to get this from charles or MJ
-  digitalWrite(8, HIGH); //start drivtrain function
-  
-}
-//*****************setup******************************end
-
-void moveScreen(){
-  for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
-    pwm.setPWM(servonum5, 0, pulselen);
-  }
-
-  delay(3000);
-  
-  for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
-    pwm.setPWM(servonum5, 0, pulselen);
-  }
-
-  delay(3000);
-}
-
-void initServo(){
-  pwm.begin();
-  /*
-   * In theory the internal oscillator (clock) is 25MHz but it really isn't
-   * that precise. You can 'calibrate' this by tweaking this number until
-   * you get the PWM update frequency you're expecting!
-   * The int.osc. for the PCA9685 chip is a range between about 23-27MHz and
-   * is used for calculating things like writeMicroseconds()
-   * Analog servos run at ~50 Hz updates, It is importaint to use an
-   * oscilloscope in setting the int.osc frequency for the I2C PCA9685 chip.
-   * 1) Attach the oscilloscope to one of the PWM signal pins and ground on
-   *    the I2C PCA9685 chip you are setting the value for.
-   * 2) Adjust setOscillatorFrequency() until the PWM update frequency is the
-   *    expected value (50Hz for most ESCs)
-   * Setting the value here is specific to each individual I2C PCA9685 chip and
-   * affects the calculations for the PWM update frequency. 
-   * Failure to correctly set the int.osc value will cause unexpected PWM results
-   */
-  pwm.setOscillatorFrequency(27000000);
-  pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
-
-  pwm.writeMicroseconds(servonum1, USMINSWIVEL);
-  pwm.writeMicroseconds(servonum, USMINROD);
-}
-
-
-
-void grabBead() {
-
-  Serial.println(servonum);
-
-  /* for (uint16_t pulselen = SERVOMIN; pulselen < 500; pulselen++) {
-      pwm.setPWM(servonum, 0, pulselen); 
-  }
-
-  delay(1000);
-  
-   for (uint16_t pulselen = SERVOMAX; pulselen < SERVOMIN; pulselen--) {
-      pwm.setPWM(servonum, 0, pulselen); 
-  }*/
-
-    //movement for swivel
-  for (uint16_t microsec = USMINSWIVEL; microsec < USMAXSWIVEL; microsec++) {
-    pwm.writeMicroseconds(servonum1, microsec);
-  }
-  
-  //movement for rod
-  for (uint16_t microsec = USMINROD; microsec < USMAXROD; microsec++) {
-    pwm.writeMicroseconds(servonum, microsec);
-  }
-
-  delay(500);
-
-  for (uint16_t microsec = USMAXSWIVEL; microsec > USDROPSWIVEL; microsec--) {
-    pwm.writeMicroseconds(servonum1, microsec);
-  }
-
-  delay(500);
-  
-  for (uint16_t microsec = USMAXROD; microsec > USDROPROD; microsec--) {
-    pwm.writeMicroseconds(servonum, microsec);
-  }
-
-
-  delay(500);
-  
-  //servonum++;
-  if (servonum > 1) servonum = 0;
-
-  
-}
-
-void extendup(){
-
-  digitalWrite(2, HIGH);
-
-  pwm.setPWM(servonum2, 500, 3500);
-
-  delay(1000);
-
-  for (uint16_t microsec = USMINROD; microsec < USVERTICALROD; microsec++) {
-    pwm.writeMicroseconds(servonum, microsec);
-  }
-
-  delay(3000);
-  
-  for (uint16_t microsec = USVERTICALROD; microsec > USSTRAIGHTROD; microsec--) {
-    pwm.writeMicroseconds(servonum, microsec);
-  }
-
-  delay(4000);
-
-  for (uint16_t microsec = USSTRAIGHTROD; microsec < USMAXROD; microsec++) {
-    pwm.writeMicroseconds(servonum, microsec);
-  }
-
-  delay(2000);
-
-  pwm.setPWM(servonum2, 1024, 1024);
-
-  for (uint16_t microsec = USMINSWIVEL; microsec > USDROPSWIVEL; microsec--) {
-    pwm.writeMicroseconds(servonum1, microsec);
-  }
-
-  for (uint16_t microsec = USMAXROD; microsec > USDROPROD; microsec--) {
-    pwm.writeMicroseconds(servonum, microsec);
-  }
-
-  delay(1000);
-
-  for (uint16_t microsec = USDROPROD; microsec > USMINROD; microsec--) {
-    pwm.writeMicroseconds(servonum, microsec);
-  }
-
-  for (uint16_t microsec = USDROPSWIVEL; microsec < USMINSWIVEL; microsec++) {
-    pwm.writeMicroseconds(servonum1, microsec);
-  }
-
-  digitalWrite(2, LOW);
-
-  pwm.setPWM(servonum2, 500, 3500);
-  
-  delay(14000);
-
-  pwm.setPWM(servonum2, 1024, 1024);
-
-  delay(1000);
-  
+  digitalWrite(READY, LOW);
 }
 
 void loop() {
- 
-  //grabBead();
-  //extendup();
+  // put your main code here, to run repeatedly:
+  
+}
 
+void launcherInit(){
+  pinMode(ENCODER_PIN,INPUT);
+  pinMode(WINCH_PIN,OUTPUT);
+  pinMode(WINCH_DIR_PIN,OUTPUT);
+  pinMode(SERVO_PIN,OUTPUT);
+  pinMode(LIMIT_SW_PIN, INPUT_PULLUP);
+  launcherServo.attach(SERVO_PIN);
+  launcherServo.write(15);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_PIN),count,FALLING);
+}
+
+void launcherArm(){
+  launcherFire();
+  //pull launcher down
+  digitalWrite(WINCH_DIR_PIN, HIGH);
+  analogWrite(WINCH_PIN, 255);
+
+  //wait for the limit switch to be hit then stop
+  while(digitalRead(LIMIT_SW_PIN) == 0);
+  analogWrite(WINCH_PIN, 0);
+
+  lock();
+  encoderValue = 0;
+  delay(2000);
+  //let out string
+  digitalWrite(WINCH_DIR_PIN, LOW);
+  analogWrite(WINCH_PIN, 255);
+  while (encoderValue < LAUNCH_VALUE);
+  analogWrite(6, 0); //stop the winch
+  armed = true;
+  
+}
+
+void launcherFire() {
+  launcherServo.write(15);
+  armed = false;
+}
+
+bool isArmed(){
+  return armed;
+}
+
+volatile void count(){
+  encoderValue++;
+}
+
+
+void lock() {
+  launcherServo.write(105);
+}
+
+void armInit(){
+  pinMode(LA_DIR_PIN, OUTPUT);
+  pinMode(LA_PWM_PIN, OUTPUT);
+
+  analogWrite(LA_PWM_PIN, 0);
+  
+  rotate.attach(ROTATE_SERVO_PIN);
+  upDown.attach(UPDOWN_SERVO_PIN);
+
+  rotate.write(ROTATE_START);
+  upDown.write(UPDOWN_START);
+}
+
+void armGrabBead(){
+
+  //it takes 8.5 seconds to reach the top so make delay not go past the
+  //.75 seconds is used in the prep
+  digitalWrite(LA_DIR_PIN, LA_UP_DIR);
+  analogWrite(LA_PWM_PIN, 254);
+  
+  for(int i = 0; i < UPDOWN_STEPS; i ++ ){
+    upDown.write(UPDOWN_TOP - i);
+    delay(40);
+  }//this will  total a delay of 2.2 seconds
+
+  delay(5550);
+  analogWrite(LA_PWM_PIN, 0);
+
+  //drop in the launcher phase
+  for(int i = 0; i < ROTATE_STEPS; i++){
+    rotate.write(ROTATE_START - i);
+    delay(10);
+  }
+
+  upDown.write(UPDOWN_DUMP);
+  delay(2000);
+
+  //reset
+  rotate.write(ROTATE_START);
+  upDown.write(UPDOWN_START);
+
+  digitalWrite(LA_DIR_PIN, LA_DOWN_DIR);
+  analogWrite(LA_PWM_PIN, 254);
+  delay(8500);
+  analogWrite(LA_PWM_PIN, 0);
+  
+}
+
+void armPrep(){
+  digitalWrite(LA_DIR_PIN, LA_UP_DIR);
+  analogWrite(LA_PWM_PIN, 254);
+  delay(750);
+  upDown.write(UPDOWN_TOP);
+  analogWrite(LA_PWM_PIN, 0);
+}
+
+void screenInit(){
+  screen.attach(SCREEN_SERVO_PIN);
+  screen.write(10);
+}
+
+void screenMove(){
+  screen.write(170);
+  delay(1000);
+  screen.write(10);
 }
